e44150788c7b1a70cd590d90297ad49c57bbb770
Sanjida Orpi <so2189@nyu.edu>
2023-04-22 11:45:04 -0400

    Initial Commit

---
+++ AboutMeApp/AboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeAppApp.swift
+//  AboutMeApp
+//
+//  Created by Sanjida Orpi on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
